217263d2d0c00f395c4c861bfe210a4d9afe7d68
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2025-01-22 17:10:40 +0000

    Add task-worker module with environment configuration and task management functionality

---
+++ task-worker/main.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+import { complete, getNextTask, heartBeat } from './tasks-client.js';
+import { program } from 'commander';
+
+program
+    .version('0.0.1')
+    .description('Task Worker CLI Tool')
+    .option('-hbi, --heartbeat-interval <interval>', 'Specify the heartbeat interval in milliseconds', 15000)  // 15 seconds
+    .parse(process.argv);
+
+const options = program.opts();
+const heartbeatInterval = options.heartbeatInterval || 15000;
+
+const logger = (taskId, message) => {
+    console.log(`[ ${new Date().toLocaleString()}${taskId ? ` ID: ${taskId}` : ``} ] ${message}`);
+};
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const doWork = async () => {
+    while (true) {
+        let task = null;
+        try {
+            task = await getNextTask();
+
+            if (!task.taskData && task.message) {
+                logger(null, `📨 ${task.message}`);
+                await sleep(5000); // Wait before checking for the next task
+                continue;
+            }
+        } catch (error) {
+            logger(null, `🌋 Error: ${error.message}`);
+            await sleep(5000); // Wait before retrying
+            continue;
+        }
+
+        let ticks = 0;
+        let heartbeatTimer = setInterval(async () => {
+            try {
+                const r = await heartBeat(task);
+                if (r.lease_expired) {
+                    logger(task.id, `🗑️ Task Lease Expired: ${r.message}; abandoning work`);
+                    clearInterval(heartbeatTimer);
+                    return;
+                }
+                task = r;
+                logger(task.id, `💓 renewed for ${(new Date(task.mustHeartBeatBefore) - new Date(task.lastHeartBeatAt)) / 1000} seconds.`);
+                logger(task.id, `⌛ Next heartbeat in ${heartbeatInterval / 1000} seconds...`);
+
+                const chaos = Math.random();
+                if (chaos < 0.1 && ticks % 2 == 0) {
+                    logger(task.id, `🙊 Simulated high latency for worker.`);
+                    const latency = Math.floor(Math.random() * (15000 - 4000 + 1)) + 4000;
+                    await sleep(latency);
+                    logger(task.id, `🔔 Resuming from latency, now working for another ${task.taskData.sleep_duration_seconds - ticks} seconds...`);
+                }
+            } catch (error) {
+                logger(null, `Error: ${error.message}`);
+                clearInterval(heartbeatTimer);
+            }
+        }, heartbeatInterval);
+
+        try {
+            logger(task.id, `🏢 Processing task ${task.id} for ${task.taskData.sleep_duration_seconds} seconds.`);
+            while (ticks < task.taskData.sleep_duration_seconds) {
+                if (ticks % 10 === 0) {
+                    logger(task.id, `💤 Working for another ${task.taskData.sleep_duration_seconds - ticks} seconds...`);
+                }
+
+                const chaos = Math.random();
+                if (chaos < 0.05 && ticks % 5 == 0) {
+                    logger(task.id, `🙊 Simulated worker failure`);
+                    process.exit(-1);
+                }
+
+                await sleep(1000);
+                ticks++;
+            }
+
+            await complete(task, { message: "done!!" });
+            logger(task.id, `🏁 Finished processing task ${task.id}`);
+        } catch (error) {
+            logger(null, `Error: ${error.message}`);
+        } finally {
+            clearInterval(heartbeatTimer);
+        }
+    }
+};
+
+doWork();
+
